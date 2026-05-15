66aa465d1dd4e57f624aebe2a5069531a9117e35
dicatop0001-maker <dicatop0001@gmail.com>
2026-05-15 17:31:25 -0300

    stage1: models/room_model + RoomType enum

---
+++ lib/models/room_model.dart
@@ -0,0 +1,45 @@
+class RoomModel {
+  final String id;
+  final String name;
+  final String type;
+  final String qrData;
+  final DateTime createdAt;
+  final String? description;
+
+  const RoomModel({
+    required this.id, required this.name, required this.type,
+    required this.qrData, required this.createdAt, this.description,
+  });
+
+  static String buildQrData(String roomId) => 'mesh://$roomId';
+
+  Map<String, dynamic> toJson() => {
+    'id': id, 'name': name, 'type': type, 'qrData': qrData,
+    'createdAt': createdAt.toIso8601String(), 'description': description,
+  };
+
+  factory RoomModel.fromJson(Map<String, dynamic> j) => RoomModel(
+    id: j['id'], name: j['name'], type: j['type'], qrData: j['qrData'],
+    createdAt: DateTime.parse(j['createdAt']), description: j['description'],
+  );
+}
+
+enum RoomType {
+  bairro('Bairro', '\u{1F3D8}'),
+  condominio('Condominio', '\u{1F3E2}'),
+  evento('Evento', '\u{1F3AA}'),
+  show('Show', '\u{1F3B5}'),
+  futebol('Futebol', '\u26BD'),
+  feira('Feira', '\u{1F6D2}'),
+  praia('Praia', '\u{1F3D6}'),
+  trilha('Trilha', '\u{1F45E}'),
+  carnaval('Carnaval', '\u{1F3AD}'),
+  emergencia('Emergencia', '\u{1F6A8}'),
+  protesto('Manifestacao', '\u270A'),
+  turismo('Turismo', '\u{1F4F8}'),
+  geral('Geral', '\u{1F4E1}');
+
+  final String label;
+  final String emoji;
+  const RoomType(this.label, this.emoji);
+}
