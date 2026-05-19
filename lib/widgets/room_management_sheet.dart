545e672481fadb6097e8339689623cf4287dd68f
dicatop0001-maker <dicatop0001@gmail.com>
2026-05-18 22:37:17 -0300

    fix: adicionar import flutter/services.dart para Clipboard

--- lib/widgets/room_management_sheet.dart
+++ lib/widgets/room_management_sheet.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 import 'dart:ui' as ui;
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:qr_flutter/qr_flutter.dart';
